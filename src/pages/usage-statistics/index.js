88fc4ff03ca393ecd68789f3ad130ed7b17667e7
Vivek Singh <petmongrels@gmail.com>
2019-10-24 13:05:31 +0530

    removed duplicate title for usage statistics

--- src/pages/usage-statistics/index.js
+++ src/pages/usage-statistics/index.js
@@ -7,10 +7,6 @@ export default function Index() {
     <Layout>
       <section className="section">
         <div className="container">
-          <h2
-            id="usage-dashboard"
-            className="title"
-          >Live usage report</h2>
           <iframe
             src="https://reporting.openchs.org/public/dashboard/de69d6d5-347f-41af-b84a-0967625bee24"
             frameBorder="0"
