373bdab23517b3e8d7f64d872d0db07603cac44b
Charles <charlesj@gmail.com>
2019-11-04 15:10:40 +0530

    Add gatsby-remark-embed-video

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,7 @@ module.exports = {
               destinationDir: 'static',
             },
           },
+          'gatsby-remark-embed-video',
           'gatsby-remark-responsive-iframe'
         ],
       },
