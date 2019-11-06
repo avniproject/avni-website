776507898ed436ec4596953e853d582fd0907118
Vivek Singh <petmongrels@gmail.com>
2019-11-06 15:13:08 +0530

    do not display test tag

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { kebabCase } from 'lodash'
+import _, { kebabCase } from 'lodash'
 import Helmet from 'react-helmet'
 import { Link, graphql } from 'gatsby'
 import Layout from '../../components/Layout'
@@ -23,20 +23,20 @@ const TagsPage = ({
           >
             <h1 className="title is-size-2 is-bold-light">Tags</h1>
             <ul className="taglist">
-              {group.map(tag => (
+              {_.filter(group, (tag) => tag.fieldValue.toLowerCase() !== "test").map(tag =>
                 <li key={tag.fieldValue}>
                   <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
                     {tag.fieldValue} ({tag.totalCount})
                   </Link>
                 </li>
-              ))}
+              )}
             </ul>
           </div>
         </div>
       </div>
     </section>
   </Layout>
-)
+);
 
 export default TagsPage
 
