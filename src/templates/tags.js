dbf1ad37acdb2864c11b09d18818f8213b6e939c
Vivek Singh <petmongrels@gmail.com>
2019-11-06 15:22:27 +0530

    tag list display

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -5,20 +5,20 @@ import Layout from '../components/Layout'
 
 class TagRoute extends React.Component {
   render() {
-    const posts = this.props.data.allMarkdownRemark.edges
+    const posts = this.props.data.allMarkdownRemark.edges;
     const postLinks = posts.map(post => (
       <li key={post.node.fields.slug}>
         <Link to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
+          <p className="is-size-5">{post.node.frontmatter.title}</p>
         </Link>
       </li>
-    ))
-    const tag = this.props.pageContext.tag
-    const title = this.props.data.site.siteMetadata.title
-    const totalCount = this.props.data.allMarkdownRemark.totalCount
+    ));
+    const tag = this.props.pageContext.tag;
+    const title = this.props.data.site.siteMetadata.title;
+    const totalCount = this.props.data.allMarkdownRemark.totalCount;
     const tagHeader = `${totalCount} post${
       totalCount === 1 ? '' : 's'
-    } tagged with “${tag}”`
+    } tagged with “${tag}”`;
 
     return (
       <Layout>
@@ -30,8 +30,10 @@ class TagRoute extends React.Component {
                 className="column is-10 is-offset-1"
                 style={{ marginBottom: '6rem' }}
               >
-                <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
+                <h2 className="title is-size-4 is-bold-light">{tagHeader}</h2>
+                  <br/>
                 <ul className="taglist">{postLinks}</ul>
+                  <br/>
                 <p>
                   <Link to="/tags/">Browse all tags</Link>
                 </p>
