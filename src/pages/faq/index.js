825010de75b4e095041170821cb70c080cab6680
Vivek Singh <petmongrels@gmail.com>
2019-12-26 10:57:25 +0530

    Created contact us component that can be used from multiple places

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -1,35 +1,44 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import ContactUs from "../../components/ContactUs";
 
 export default function Index() {
-  return (
-      <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1>FAQ (frequently asked questions)</h1>
-            </div>
-            <br/>
-            <div className="content">
-              <h3>I do not know how to setup Avni myself, how to I find the service provider?</h3>
-              <p>Currently Samanvay also does implementation of Avni. Avni team is in process of on boarding other service providers. But if you know some service
-                provider that may be interested in implementing Avni for you then Avni team can help them too.
-              </p>
-            </div>
+    return (
+        <Layout>
+            <section className="section">
+                <div className="container">
+                    <div className="content">
+                        <h1>FAQ (frequently asked questions)</h1>
+                    </div>
+                    <br/>
+                    <div className="content">
+                        <h3>I do not know how to setup Avni myself, how to I find the service provider?</h3>
+                        <p>Currently Samanvay also does implementation of Avni. Avni team is in process of on boarding other service providers. But if you know some
+                            service
+                            provider that may be interested in implementing Avni for you then Avni team can help them too.
+                        </p>
+                    </div>
 
-            <div className="content">
-              <h3>How much would an implementation of Avni cost?</h3>
-              <p>Avni is open source so there is no license cost for the software. But there are other costs for - implementation, hosting and ongoing user support.
-                Implementation is usually one time activity of configuring application as per your needs on the platform. When you want to start using the application
-                then you will need hosting and perhaps ongoing user support. In order to understand the details of these cost please contact us
-                avni-project@googlegroups.com - as the cost depends on the complexity of the implementation, number of users and workloads.
-              </p>
-            </div>
-          </div>
-        </section>
-        <br/>
-        <br/>
-        <br/>
-      </Layout>
-  )
+                    <div className="content">
+                        <h3>How much would an implementation of Avni cost?</h3>
+                        <p>Avni is open source so there is no license cost for the software. But there are other costs for - implementation, hosting and ongoing user
+                            support.
+                            Implementation is usually one time activity of configuring application as per your needs on the platform. When you want to start using the
+                            application
+                            then you will need hosting and perhaps ongoing user support. In order to understand the details of these cost please contact us
+                            avni-project@googlegroups.com - as the cost depends on the complexity of the implementation, number of users and workloads.
+                        </p>
+                    </div>
+
+                    <div className="content">
+                        <h3>I want to implement Avni, where should I start from</h3>
+                        <p>The getting started page is for this purpose. You can start from there and go through the documentation. The documentation currently is to only get you started, after which you please contact us on <a href="https://gitter.im/OpenCHS/openchs" target="_blank">Gitter channel</a> or using the Contact us form below.</p>
+                    </div>
+                </div>
+            </section>
+            <br/>
+            <br/>
+            <ContactUs/>
+        </Layout>
+    )
 }
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -7,6 +7,7 @@ import Features from '../components/Features'
 import LandingPageHero from "../components/LandingPageHero";
 import {VerticalTileGroup} from "../components/Tile";
 import social_alpha_logo from '../img/SocialAlphaLogo.png'
+import ContactUs from "../components/ContactUs";
 
 export const IndexPageTemplate = ({
                                       features,
@@ -55,40 +56,7 @@ export const IndexPageTemplate = ({
                             </div>
                         </VerticalTileGroup>
                     </div>
-
-                    <h1 className="has-text-centered is-size-3-mobile is-size-2-desktop has-text-weight-bold">Contact Us</h1>
-                    <p className="has-text-centered has-text-weight-bold">Feedback, Query, Suggestion, Anything? Please send your message. We will get back to you asap!</p>
-                    <br/>
-                    <div style={{width: "50%", margin: "auto"}}>
-                        <form method="post" action="https://formspree.io/mrgbyoqr">
-                            <div>
-                                <label className="has-text-weight-bold">Message:</label>
-                                <div style={{margin: "auto"}}>
-                                    <textarea id="message" name="message" maxLength="6000" rows="7" style={{width: "100%"}}/>
-                                </div>
-                            </div>
-                            <div className="has-text-left has-text-weight-bold" style={{width: "100%", display: "table"}}>
-                                <div style={{width: "50%", float: "left"}}>
-                                    <label>Your Name:</label>
-                                    <div>
-                                        <input type="text" className="form-control" id="name" name="name" required style={{width: "95%"}}/>
-                                    </div>
-                                </div>
-                                <div style={{width: "50%", float: "left"}}>
-                                    <label>Email:</label>
-                                    <div>
-                                        <input type="email" className="form-control" id="email" name="_replyto" required style={{width: "100%"}}/>
-                                    </div>
-                                </div>
-                            </div>
-                            <br/>
-                            <div className="has-text-centered">
-                                <div className="">
-                                    <button type="submit" className="btn">Send &rarr;</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                    <ContactUs/>
                 </div>
             </div>
         </section>
