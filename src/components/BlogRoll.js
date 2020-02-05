b343d063dac1b23f6bf4e7eb03b4cf5ebc1b6690
Vivek Singh <petmongrels@gmail.com>
2020-02-05 17:28:26 +0530

    changing display of blogs

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -5,8 +5,8 @@ import PreviewCompatibleImage from './PreviewCompatibleImage'
 
 class BlogRoll extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <div className="columns is-multiline">
@@ -66,7 +66,7 @@ BlogRoll.propTypes = {
       edges: PropTypes.array,
     }),
   }),
-}
+};
 
 export default () => (
   <StaticQuery
