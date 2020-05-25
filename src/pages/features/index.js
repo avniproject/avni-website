8185faf6f05c9750eea31535fbd47b70f82c5183
Vivek Singh <petmongrels@gmail.com>
2020-05-25 12:20:45 +0530

    Moved use cases to features page, to make space for more menu items. Added pricing page.

--- src/pages/features/index.js
+++ src/pages/features/index.js
@@ -139,9 +139,7 @@ export default function Index() {
                         className="container"
                         style={{marginTop: "2em"}}
                     >
-                        <h3 className="has-text-weight-semibold title">
-                            Health specific features
-                        </h3>
+                        <h5 className="has-text-weight-semibold">Health specific features</h5>
                         <p>
                             Avni has certain features, that can be turned on, for health
                             implementations.
@@ -164,6 +162,71 @@ export default function Index() {
                                     images={[
                                         {src: imgChildImmunisation, altText: "Child immunisation management"}
                                     ]}
+                                />
+                            </VerticalTileGroup>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <section className="section">
+                <div className="container">
+                    <div className="content">
+                        <h1>Use Cases</h1>
+                        <p>Most social sector work, done by NGO and Governments ultimately rests on the field workers providing service to the communities. Avni aims to
+                            support almost all field work.</p>
+                    </div>
+                </div>
+                <div className="container" style={{marginTop: '2em'}}>
+                    <div className="tile is-ancestor">
+                        <VerticalTileGroup>
+                            <FeatureTile
+                                title="Service delivery"
+                                description="Organisations provide direct services to the beneficiaries they serve (e.g. community health service)"
+                            />
+                        </VerticalTileGroup>
+                        <VerticalTileGroup>
+                            <FeatureTile
+                                title="Service facilitation"
+                                description="Helping beneficiaries in accessing the services provided by government or large institutions (e.g. ensuring institutional delivery, enrolling in government programmes)"
+                            /></VerticalTileGroup>
+                        <VerticalTileGroup>
+                            <FeatureTile
+                                title="Data collection"
+                                description="Collect data on an individual/object (e.g. learning assessment of students, water body servicing details)."
+                            />
+                        </VerticalTileGroup>
+                    </div>
+                    <div id="health-modules" className="container" style={{marginTop: '2em'}}>
+                        <h4 className="has-text-weight-semibold" style={{lineHeight: '4em'}}>
+                            Following are some of the existing use cases of Avni. Also check out <a href="/case-studies">case studies</a> and <a href="/usage-statistics">usage
+                            statistics</a>.
+                        </h4>
+                        <div className="tile is-ancestor">
+                            <VerticalTileGroup>
+                                <FeatureTile
+                                    title="Maternal and Child Health"
+                                    description="In health this has been the most common use-case. Avni for MCH has been used primarily at the ASHA/Village/Slum level for service delivery. For maternal program the usage spans from conception to PNC."
+                                    detailsLabel="Case study"
+                                    detailsLink="/case-studies/calcutta-kids-%E2%80%94-avni-implemented-for-maternal-and-child-health-program"
+                                />
+                                <FeatureTile
+                                    title="Sickle Cell Screening and Treatment Initiation"
+                                    description="Avni helped manage the process of screening population for sickle cell disease, conducting lab tests, track lab results and connect with treatment if required."
+                                />
+                            </VerticalTileGroup>
+                            <VerticalTileGroup>
+                                <FeatureTile title="Common Cancer Screening and Referral"
+                                             description="Avni was used to screen people for common cancers (oral, breast and cervical) and refer them for hospital examination if found suspect."/>
+                                <FeatureTile title="Adolescent care"
+                                             description="Track health, personal development and risk indicators of male and female adolescents - in schools and villages. Provide counselling, health advice and iron supplements for those identified as vulnerable or at risk."/>
+                            </VerticalTileGroup>
+                            <VerticalTileGroup>
+                                <FeatureTile title="Monitoring of dam de-silting work"
+                                             description="Collect data to help in monitoring the progress of dam de-silting work happening in multiple districts."
+                                             detailsLabel="Case study"
+                                             detailsLink="/case-studies/dam-and-water-bodies-desilting-work-monitoring-1"
                                 />
                             </VerticalTileGroup>
                         </div>
---
+++ src/pages/pricing/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+export default function Index() {
+    return (
+        <Layout>
+            <br/>
+            <div className="container">
+                <h1 className="title">Avni Plans</h1>
+                <p>We are offering one time special plan for the first 50 organisations. If you have a higher number of users, or expected forms submissions, or you need any services like support/implementation/training, or any other reason - please use the custom plan to contact us.</p>
+                <div className="columns is-mobile is-multiline">
+                    <div className="column is-half-tablet is-10-mobile is-offset-1-mobile">
+                        <br/>
+                        <br/>
+                        <div className="content has-background-grey-lighter" style={{padding: '2em'}}>
+                            <h1>First 50</h1>
+                            <p><i>22 signed up so far</i></p>
+                            <br/>
+                            <p><b>Flat Rs. 30000 annually, including tax</b></p>
+                            <p><b>Up to 100 users or 10,000 form submissions per month</b></p>
+                            <p><b>Billed one time</b></p>
+                            <p><b>Free 3 months trial</b></p>
+                            <p><b>Community support</b></p>
+                            <br/>
+
+                            <p className="control full-centered">
+                                <a href="/signup" className="is-secondary button"><b>SIGN UP</b></a>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="column is-half-tablet is-10-mobile is-offset-1-mobile">
+                        <br/>
+                        <br/>
+                        <div className="content has-background-grey-lighter" style={{padding: '2em'}}>
+                            <h1>Customised plan</h1>
+                            <br/>
+                            <p><i>&nbsp;</i></p>
+                            <p><b>Annual charges based on your requirements</b></p>
+                            <p><b>Billed one or multiple times in a year</b></p>
+                            <p><b>Free 3 months trial</b></p>
+                            <p><b>Community support</b></p>
+                            <p><b>Services - implementation, support, training, product enhancements</b></p>
+                            <br/>
+                            <p className="control full-centered">
+                                <a href="/signup" className="is-secondary button"><b>CONTACT SALES</b></a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <br/>
+            </div>
+            <br/>
+            <br/>
+        </Layout>
+    )
+}
