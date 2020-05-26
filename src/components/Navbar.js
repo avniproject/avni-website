21c9b5422584b099d5d6f5a391589b0318c49d9a
Vivek Singh <petmongrels@gmail.com>
2020-05-26 16:47:00 +0530

    open app in the same tab

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -119,7 +119,7 @@ export default class Navbar extends React.Component {
                                             <div className="navbar-item">
                                                 <div className="field is-grouped is-grouped-multiline">
                                                     <p className="control">
-                                                        <a href="https://app.avniproject.org" className="button" target="_blank">LOGIN</a>
+                                                        <a href="https://app.avniproject.org" className="button">LOGIN</a>
                                                     </p>
                                                     <p className="control">
                                                         <a href={`/signup?${Constants.ContactSource}=${Constants.Trial}`} className="is-primary button">TRY FOR FREE</a>
