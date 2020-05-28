61a7e0e33b6ea57e266718994b9ce2044a75ca02
Vinay Venu <ijkvinay@gmail.com>
2020-05-28 15:44:44 +0530

    Privacy page

---
+++ src/pages/privacy/index.js
@@ -0,0 +1,142 @@
+import React from "react";
+
+export default function Index() {
+    return (
+        <div class={"content"}>
+            <section class="section">
+                <p className={"title is-3"}>Privacy Policy</p>
+                <span><b>Posted</b>: May 28, 2020</span>
+                <p><b>Effective</b>: May 28, 2020</p>
+                <p>This notice describes the Privacy Policy for the usage of services provided around the Avni product
+                    hosted by Samanvay Research and Development Foundation ("Samanvay," "we", "us", or "our"). Here we
+                    describe how we collect, use and handle your information when you use our websites, software and
+                    services ("Services").</p>
+
+                <p>If you're using our Services for an organization, you're agreeing to this Privacy Policy on behalf of
+                    that organization.
+                    If you're using our Services to provide services, software and content for some else, you're
+                    agreeing to this Privacy Policy on their behalf.
+                    IF YOU OR THE ORGANIZATION YOU REPRESENT DON'T AGREE TO BE BOUND TO THIS PRIVACY POLICY, YOU SHOULD
+                    NOT
+                    USE OUR SERVICES.</p>
+            </section>
+            <section className={"section"}>
+                <p className={"subtitle is-5"}>Data collected by us</p>
+                <p>We collect and use the following information to provide, improve and protect our Services:</p>
+
+                <p><b>1. Account Data: </b>
+                    When registering to use our Services, Samanvay requires you to provide your personal contact
+                    information, such as name, company name, address, phone number, and email address ("Contact
+                    Information"). When purchasing our Services, Samanvay may require you to provide financial
+                    qualification and billing information, such as billing name and address, and the number of employees
+                    that will be using the product or services ("Billing Information"). Samanvay may also ask you to
+                    provide additional information, such as number of employees, or industry ("Optional Information").
+                    Contact Information, Billing Information, and Optional Information about customers are referred to
+                    collectively as "Account Data" We use Account Data to perform the Services requested.</p>
+
+                <p><b>2. Your Content: </b>When you use our Services, you provide us with things like your data,
+                    reports, messages, contacts, images and so on ("Your Content"). Our Services are designed to make it
+                    simple for you to collect and manage Your Content, share with others, and work across multiple
+                    devices. To make that possible, we store, process, and transmit Your Content, as well as information
+                    related to it. This related information can be things like your Contact Information that makes it
+                    easier to collaborate and share Your Content with others.</p>
+
+                <p><b>3. Usage: </b>We collect information related to how you use the Services, including actions you
+                    take in your account. This is used to conduct research and development for the further development
+                    of our Services in order to provide you a more intuitive experience, and also to provide you
+                    support.</p>
+
+                <p><b>4. Device Information: </b>We also collect information from and about the devices you use to
+                    access the Services. This includes things like IP addresses and the type of browser and device you
+                    use. Your devices (depending on their settings) may also transmit location information to the
+                    Services. This helps us improve our Services and provide you support.</p>
+
+                <p><b>5. Cookies and other technologies: </b>We use technologies like cookies and web beacons to
+                    provide, improve, and protect our Services. For example, cookies help us with things like
+                    remembering your username for your next visit, understanding
+                    how you are interacting with our Services, and improving them based on that information. You can set
+                    your browser to not accept cookies, but this may limit your ability to use the Services.</p>
+
+                <p><b>6. Aggregate Data Sets: </b>Samanvay may also use Your Content to create aggregate, anonymized
+                    data sets ("Data Sets"). Data Sets are not associated with any individual or user account, and are
+                    thus anonymous information. Samanvay may use
+                    these Aggregate Data Sets to improve our Services and for research purposes. If you do not consent
+                    to the use of Your Data to create Data Sets, please "opt-out" by contacting us at
+                    support@samanvayfoundation.org and your data will not be included as of the date of notification.
+                </p>
+            </section>
+            <section className={"section"}>
+                <p className={"subtitle is-5"}>With whom do we share</p>
+                <p>We may share information as discussed below, but we will never sell it to advertisers or other third
+                    parties.</p>
+                <p><b>1. Others working for Samanvay: </b>
+                    Samanvay uses certain trusted third parties (for example,
+                    providers of IT services) to help us
+                    provide,
+                    improve, protect, and promote our Services. These third parties will access your information only to
+                    perform tasks on our behalf in compliance with this Privacy Policy.</p>
+
+                <p><b>2. Analytics: </b>
+                    We use Google Analytics for the website https://avniproject.org. This is to help understand traffic
+                    trends, and will not be used for any other purpose.</p>
+
+                <p><b>3. Other applications: </b>
+                    You can also give third parties access to your information - for example, via our APIs. Just
+                    remember
+                    that their use of your information will be governed by their privacy policies and terms.</p>
+
+                <p><b>4. Legal Compliance and Protection: </b>
+                    We may disclose your information to third parties if we determine that such disclosure is reasonably
+                    necessary to (a) comply with the law; (b) protect any person from death or serious bodily injury;
+                    (c)
+                    prevent fraud or abuse of Samanvay or our users; or (d) protect Samanvay's property rights.</p>
+
+            </section>
+            <section className={"section"}>
+                <p className={"subtitle is-5"}>How do we protect your information</p>
+
+                <p>Samanvay acknowledges your trust and is committed to protecting the information you provide to us. To
+                    prevent unauthorized access, maintain accuracy, and ensure proper use of information, we have
+                    employed physical, technical, and administrative processes to safeguard and secure the information
+                    we collect.</p>
+
+                <p>We have also employed physical, technical, and administrative processes to detect and investigate
+                    information breaches. In the unfortunate event of a breach, we will inform all affected accounts as
+                    soon as practicable upon discovery of such a breach, consistent with any applicable legal
+                    obligations and the needs of law enforcement.</p>
+
+                <p>No method of transmission over the Internet, or method of electronic storage, is 100% secure.
+                    Therefore, while we strive to use commercially acceptable means to protect Personal Information, we
+                    cannot guarantee its absolute security or confidentiality. If you have any questions about security,
+                    you can contact us at support@samanvayfoundation.org.</p>
+
+                <p>Please be aware that certain Personal Information and other information provided by you in connection
+                    with your use of the Services may be stored on your device (even if we do not collect that
+                    information). You are solely responsible for maintaining the security of your device from
+                    unauthorized access.</p>
+            </section>
+
+            <section className={"section"}>
+                <p className={"subtitle is-5"}>Where is the information stored, processed and transmitted</p>
+
+                <p>We store and process Your Content exclusively at our data center in Mumbai, India.
+                    Your Content can be transmitted to anywhere in the world to those authorized by you or your
+                    organisation.
+                    Storing and processing of Account Data, Device information, Cookies and Aggregate Data Sets can
+                    happen anywhere in the world.
+                </p>
+            </section>
+            <section className={"section"}>
+                <p className={"subtitle is-5"}>Change Management</p>
+
+                <p>If we are involved in a reorganization, merger, acquisition or sale of our assets, your information
+                    may be transferred as part of that deal. We will notify you (for example, via a message to the email
+                    address associated with your account) of any such deal and outline your choices in that event.</p>
+
+                <p>We may revise this Privacy Policy from time to time, and will post the most current version on our
+                    website. If a revision meaningfully reduces your rights and/or requires your explicit acceptance, we
+                    will notify you via email or via in-product user experience.</p>
+            </section>
+        </div>
+    )
+}
