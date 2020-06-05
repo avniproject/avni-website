d35e6cb0f196dfe92bea439cc2cc87fb13fb234a
Vivek Singh <petmongrels@gmail.com>
2020-06-05 11:28:46 +0530

    free trial as its own block

--- src/pages/pricing/index.js
+++ src/pages/pricing/index.js
@@ -8,20 +8,37 @@ export default function Index() {
             <br/>
             <div className="container">
                 <h1 className="title">Avni Plans</h1>
-                <p>Avni is fully open source, and can be <a href="https://avni.readme.io/docs/environment-setup-guides" target="_blank">setup and used for free</a>. But
-                    if you want to use our hosted version so that you don't have to manage / maintain it, then you can look at the plans below. We are offering a one time
-                    special plan for the first 50 organisations that sign up. If you have a higher number of users, or expected forms submissions, or additional needs -
-                    please use the custom plan to contact us. Our hosted option has the following benefits:
-                </p>
-                <div style={{paddingLeft: '1em', paddingTop: '1em'}}>
-                    <li>No infrastructure and server management required</li>
-                    <li>All upgrades and bug-fixes automatically applied</li>
-                    <li>Security and data backup/recovery are all taken care of</li>
+                <div className="columns is-mobile is-multiline">
+                    <div className="column is-half-tablet is-10-mobile is-offset-1-mobile">
+                        <p>Avni is fully open source, and can be <a href="https://avni.readme.io/docs/environment-setup-guides" target="_blank">setup and used for
+                            free</a>.
+                            But if you want to use our hosted version so that you don't have to manage / maintain it, then you can look at the plans. We are offering a
+                            one time special plan for the first 50 organisations that sign up. If you have a higher number of users, or expected forms submissions, or
+                            additional needs - please use the custom plan to contact us. Our hosted option has the following benefits:
+                        </p>
+                        <div style={{paddingLeft: '1em', paddingTop: '1em'}}>
+                            <li>No infrastructure and server management required</li>
+                            <li>All upgrades and bug-fixes automatically applied</li>
+                            <li>Security and data backup/recovery are all taken care of</li>
+                        </div>
+                    </div>
+                    <div className="column is-half-tablet is-10-mobile is-offset-1-mobile">
+                        <div className="content has-background-grey-lighter" style={{padding: '2em'}}>
+                            <h1>3 Months Free Trial</h1>
+                            <br/>
+                            <li><b>Up to 100 users or 10,000 form submissions per month</b></li>
+                            <li><b>No Credit Card Required</b></li>
+                            <li><b>Community support</b></li>
+                            <br/>
+
+                            <p className="control full-centered">
+                                <a href={`/signup?${Constants.ContactSource}=${Constants.Trial}`} className="is-secondary button"><b>SIGN UP</b></a>
+                            </p>
+                        </div>
+                    </div>
                 </div>
                 <div className="columns is-mobile is-multiline">
                     <div className="column is-half-tablet is-10-mobile is-offset-1-mobile">
-                        <br/>
-                        <br/>
                         <div className="content has-background-grey-lighter" style={{padding: '2em'}}>
                             <h1>First 50</h1>
                             <p><i><a href="https://reporting.avniproject.org/public/dashboard/3b77176c-b304-4e1e-a07c-936080c0e325" target="_blank">22 signed up so
@@ -30,7 +47,7 @@ export default function Index() {
                             <li><b>Flat Rs. 30,000 annually, including tax</b></li>
                             <li><b>Up to 100 users or 10,000 form submissions per month</b></li>
                             <li><b>Billed one time</b></li>
-                            <li><b>Subscription starts after 3 months of free trial</b></li>
+                            <li><b>Free 3 months trial (no credit card)</b></li>
                             <li><b>Community support</b></li>
                             <br/>
 
@@ -41,15 +58,13 @@ export default function Index() {
                     </div>
 
                     <div className="column is-half-tablet is-10-mobile is-offset-1-mobile">
-                        <br/>
-                        <br/>
                         <div className="content has-background-grey-lighter" style={{padding: '2em'}}>
                             <h1>Customised plan</h1>
                             <br/>
                             <p><i>&nbsp;</i></p>
                             <li><b>Annual charges based on your requirements</b></li>
                             <li><b>Billed one or multiple times in a year</b></li>
-                            <li><b>Free 3 months trial</b></li>
+                            <li><b>Free 3 months trial (no credit card)</b></li>
                             <li><b>Community support</b></li>
                             <li><b>Services - implementation, support, training, product enhancements</b></li>
                             <br/>
