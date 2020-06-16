ae79568a7ebd401a355e76b8187c21205c155f25
Vivek Singh <petmongrels@gmail.com>
2020-06-16 12:57:45 +0530

    user to remain on the same page after pressing send

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,13 +1,49 @@
-import React from 'react'
+import React from 'react';
 
-const ContactUs = () => (
-    <span>
+class Status {
+    static Success = "SUCCESS";
+    static Error = "ERROR";
+    static Initial = "";
+    static Processing = "PROCESSING";
+}
+
+export default class ContactUs extends React.Component {
+    constructor(props) {
+        super(props);
+        this.submitForm = this.submitForm.bind(this);
+        this.state = {
+            status: Status.Initial
+        };
+    }
+
+    submitForm(ev) {
+        ev.preventDefault();
+        const form = ev.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                form.reset();
+                this.setState({status: Status.Success});
+            } else {
+                this.setState({status: Status.Error});
+            }
+        };
+        xhr.send(data);
+        this.setState({status: Status.Processing});
+    }
+
+    render() {
+        return <span>
         <h1 className="has-text-centered is-size-3-mobile is-size-2-desktop has-text-weight-bold">Contact Us</h1>
         <p className="has-text-centered has-text-weight-bold">Feedback, Query, Suggestion, Anything? Please send your message. We will get back to you asap!</p>
         <br/>
         <div style={{width: "65%", margin: "auto"}}>
             {/*<form method="post" action="https://formspree.io/mrgbyoqr">*/}
-            <form method="post" action="https://formspree.io/xlepnyzk">
+            <form method="post" action="https://formspree.io/xlepnyzk" onSubmit={this.submitForm}>
                 <div>
                     <label className="has-text-weight-bold">Message:</label>
                     <div style={{margin: "auto"}}>
@@ -29,16 +65,27 @@ const ContactUs = () => (
                     </div>
                 </div>
                 <br/>
+
                 <div className="has-text-centered">
                     <div className="">
-                        <button type="submit" className="btn">Send &rarr;</button>
+                        {this.getStatusDisplay()}
                     </div>
                 </div>
             </form>
         </div>
     </span>
-);
+    }
 
-ContactUs.propTypes = {};
+    getStatusDisplay() {
+        const {status} = this.state;
+        if (status === Status.Initial) return this.getSubmitButton();
+        if (status === Status.Processing) return <button className="btn is-disabled">Sending...</button>;
+        if (status === Status.Success) return <div>{this.getSubmitButton()}<p className="has-text-centered is-size-3-mobile is-size-3-desktop">Thanks
+            for contacting us! We will contact you within 1 working day.</p></div>;
+        if (status === Status.Error) return <div>{this.getSubmitButton()}<p className="has-text-centered is-size-3-mobile is-size-3-desktop has-text-weight-bold">There was an error. We are sorry, please try again.</p></div>;
+    }
 
-export default ContactUs;
+    getSubmitButton() {
+        return <button type="submit" className="btn">Send &rarr;</button>;
+    }
+}
