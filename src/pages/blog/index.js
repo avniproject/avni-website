3d251dc2fbdfceec331e603eecd03767d71243df
Vivek Singh <petmongrels@gmail.com>
2020-07-10 17:25:37 +0530

    linked to tech4dev avni blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -4,29 +4,30 @@ import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
 
 export default class BlogIndexPage extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <div className="margin-top-0">
-          <h1
-            className="is-size-1"
-            style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
-              color: 'white',
-              padding: '1rem',
-                paddingLeft: '3rem'
-            }}
-          >Avni Blog</h1>
-        </div>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <BlogRoll/>
-            </div>
-          </div>
-        </section>
-      </Layout>
-    )
-  }
+    render() {
+        return (
+            <Layout>
+                <div className="margin-top-0">
+                    <h1
+                        className="is-size-1"
+                        style={{
+                            boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+                            backgroundColor: '#f40',
+                            color: 'white',
+                            padding: '1rem',
+                            paddingLeft: '3rem'
+                        }}
+                    >Avni Blog</h1>
+                </div>
+                <section className="section">
+                    <div className="container">
+                        <div className="content">
+                            <p className="has-text-weight-bold"><a href="https://chintugudiya.org/tag/avni/" target="_blank">Tech4Dev community blog of Avni</a></p>
+                            <BlogRoll/>
+                        </div>
+                    </div>
+                </section>
+            </Layout>
+        )
+    }
 }
