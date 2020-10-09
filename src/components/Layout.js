f1a0815e1fdf4807ce58caa5e1806dd53ae931ef
Arjun <arjunk@thoughtworks.com>
2020-10-09 19:03:54 +0530

    Adding favicon back

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,6 +10,32 @@ const TemplateWrapper = ({hero, children}) => {
     const {title, description} = useSiteMetadata();
     return (
         <div>
+            <Helmet>
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href={`${withPrefix("/")}img/avni-logo-wo-name.png`}
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href={`${withPrefix("/")}img/favicon-32x32.png`}
+                    sizes="32x32"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href={`${withPrefix("/")}img/favicon-16x16.png`}
+                    sizes="16x16"
+                />
+
+                <link
+                    rel="mask-icon"
+                    href={`${withPrefix("/")}img/avni-logo-wo-name.png`}
+                    color="#ff4400"
+                />
+                <meta name="theme-color" content="#fff"/>
+            </Helmet>
             {hero ? hero : <Navbar/>}
             <div>{children}</div>
             <Footer/>
