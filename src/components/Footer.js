fa0ad053e510a35718ab83982c421813961fec7f
Vinay Venu <ijkvinay@gmail.com>
2024-06-05 11:07:19 +0530

    Remove Twitter, add LinkedIn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import GitHubLogo from '../img/social/GitHub.png';
-import TwitterLogo from '../img/social/twitter.svg';
+// import TwitterLogo from '../img/social/twitter.svg';
 import YouTubeLogo from '../img/social/youtube.png';
 import PlayStoreLogo from '../img/social/playstore-icon.png';
 import ExternalLink from "./ExternalLink";
 import Icons8Icon from '../img/attributions/icons8.png';
+import LinkedinIcon from '../img/social/linkedin-logo.png';
 
 const Footer = class extends React.Component {
     render() {
@@ -21,14 +22,17 @@ const Footer = class extends React.Component {
                     <ExternalLink href="https://github.com/avniproject">
                         <img alt="GitHub" src={GitHubLogo} style={{height: 48, width: 48}}/>
                     </ExternalLink>
-                    <ExternalLink href="https://twitter.com/avniproject">
-                        <img alt="Twitter" src={TwitterLogo} style={{marginLeft: 10, height: 48, width: 48}}/>
-                    </ExternalLink>
+                    {/*<ExternalLink href="https://twitter.com/avniproject">*/}
+                    {/*    <img alt="Twitter" src={TwitterLogo} style={{marginLeft: 10, height: 48, width: 48}}/>*/}
+                    {/*</ExternalLink>*/}
                     <ExternalLink href="https://www.youtube.com/channel/UCShsfKJlw0B3B6Pg2DmQkSQ">
                         <img alt="YouTube" src={YouTubeLogo} style={{height: 48, width: 67, marginLeft: 10}}/>
                     </ExternalLink>
                     <ExternalLink href="https://play.google.com/store/apps/details?id=com.openchsclient">
                         <img alt="Play Store" src={PlayStoreLogo} style={{height: 48, width: 48, marginLeft: 10}}/>
+                    </ExternalLink>
+                    <ExternalLink href="https://www.linkedin.com/showcase/avniproject">
+                        <img alt="LinkedIn" src={LinkedinIcon} style={{height: 48, width: 48, marginLeft: 10}}/>
                     </ExternalLink>
                 </div>
                 <div className="has-text-left">
