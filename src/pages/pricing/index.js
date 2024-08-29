7e0afadfe2734e2004f277fcf1c53ed492be728e
Vinay Venu <ijkvinay@gmail.com>
2024-08-29 18:01:42 +0530

    Change plan details

--- src/pages/pricing/index.js
+++ src/pages/pricing/index.js
@@ -1,68 +1,83 @@
-import React from "react";
-import Layout from "../../components/Layout";
-import Constants from "../../Constants";
-import ExternalLink from "../../components/ExternalLink";
+import React from 'react';
+import Layout from '../../components/Layout';
+import Constants from '../../Constants';
+import ExternalLink from '../../components/ExternalLink';
 
 export default function Index() {
     return (
         <Layout>
             <br/>
             <div className="container">
+                <h1 className="title">Avni Plans</h1>
+                <br/>
+                <p>Avni is fully open source, and can be <ExternalLink
+                    href="https://avni.readme.io/docs/environment-setup-guides">setup and used for
+                    free</ExternalLink>. These plans are meant if you choose to host your application on the Avni
+                    cloud, or if you would want to be trained on Avni.
+                </p>
+                <br/>
+                <h1 className="is-size-5 has-text-weight-bold">Avni App and Data Analytics Development</h1>
+                <br/>
                 <div className="columns is-mobile is-multiline">
-                    <div className="column is-10-mobile is-offset-1-mobile">
-                        <h1 className="title">Avni Plans</h1>
-                        <p>Avni is fully open source, and can be <ExternalLink href="https://avni.readme.io/docs/environment-setup-guides">setup and used for
-                            free</ExternalLink>.
-                            But if you want to use our cloud hosted version so that you don't have to manage / maintain it, then you can look at the plans. We are offering a
-                            one time special plan for the first 50 organisations that sign up. If you have a higher number of users, or expected forms submissions, or
-                            additional needs - please use the custom plan to contact us. Our hosted option has the following benefits:
-                        </p>
-                        <div style={{paddingLeft: '1em', paddingTop: '1em'}}>
-                            <li>No infrastructure and server management required</li>
-                            <li>All upgrades and bug-fixes automatically applied</li>
-                            <li>Security and data backup/recovery are all taken care of</li>
+                    <div className="column is-one-third-tablet is-10-mobile is-offset-1-mobile">
+                        <div className="content has-background-grey-lighter" style={{padding: '1em'}}>
+                            <p className="is-size-6 has-text-weight-bold">Self Service</p>
+                            <ol>
+                                <li>Signup for an account</li>
+                                <li>Develop your app and reports</li>
+                                <li>Use online community support channel for support</li>
+                                <li>One month of free hosting</li>
+                            </ol>
+                            <p className="has-text-weight-semibold">Pricing: Free</p>
+                        </div>
+                    </div>
+                    <div className="column is-one-third-tablet is-10-mobile is-offset-1-mobile">
+                        <div className="content has-background-grey-lighter" style={{padding: '1em'}}>
+                            <p className="is-size-6 has-text-weight-bold">Self Service with Training</p>
+                            <ol>
+                                <li>12 hours of Avni low-code platform live training</li>
+                                <li>8 hours of consulting support over the next 1 month</li>
+                                <li>Develop your app and reports</li>
+                            </ol>
+                            <p className="has-text-weight-semibold">Pricing: ₹40,000/-</p>
+                        </div>
+                    </div>
+                    <div className="column is-one-third-tablet is-10-mobile is-offset-1-mobile">
+                        <div className="content has-background-grey-lighter" style={{padding: '1em'}}>
+                            <p className="is-size-6 has-text-weight-bold">Samanvay Serviced Implementation</p>
+                            <ol>
+                                <li>We help you build Avni to your needs</li>
+                                <li>We support you in migration from an existing system or integrating to other systems</li>
+                                <li>Training of master trainers</li>
+                            </ol>
+                            <p className="has-text-weight-semibold">Pricing: Custom Quote</p>
                         </div>
                     </div>
                 </div>
-                <div className="columns is-mobile is-multiline">
-                    <div className="column is-half-tablet is-10-mobile is-offset-1-mobile">
-                        <div className="content has-background-grey-lighter" style={{padding: '2em'}}>
-                            <h1>3 Months Free Trial</h1>
-                            <br/>
-                            <li><b>All features</b></li>
-                            <li><b>Up to 100 users or 10,000 form submissions per month</b></li>
-                            <li><b>Multiple projects</b></li>
-                            <li><b>No Credit Card Required</b></li>
-                            <li><b>Community support</b></li>
-                            <br/>
-
-                            <p className="control full-centered">
-                                <a href={`/signup?${Constants.ContactSource}=${Constants.Trial}`} className="is-secondary button"><b>SIGN UP</b></a>
-                            </p>
-                        </div>
-                    </div>
-                    <div className="column is-half-tablet is-10-mobile is-offset-1-mobile">
-                        <div className="content has-background-grey-lighter" style={{padding: '2em'}}>
-                            <h1>Customised plan</h1>
-                            <br/>
-                            <p><i>&nbsp;</i></p>
-                            <li><b>All features</b></li>
-                            <li><b>Annual charges based on your requirements</b></li>
-                            <li><b>Billed one or multiple times in a year</b></li>
-                            <li><b>Free 3 months trial (no credit card)</b></li>
-                            <li><b>Community support</b></li>
-                            <li><b>Services - implementation, support, training, product enhancements</b></li>
-                            <br/>
-                            <p className="control full-centered">
-                                <a href={`/signup?${Constants.ContactSource}=${Constants.CustomPlan}`} className="is-secondary button"><b>POST ENQUIRY</b></a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                <br/>
+                <h1 className="is-size-5 has-text-weight-bold">Avni Cloud Hosting</h1>
+                <p>Once you develop your application, it can be hosted on the Avni cloud. See the plans below to get a better understanding of your options.</p>
+                <br/>
+                <p className="is-size-6 has-text-weight-semibold">What's included</p>
+                <ol>
+                    <li>Hosting of your application on the Avni cloud</li>
+                    <li>Regular upgrades of Avni and other components</li>
+                    <li>Data backups</li>
+                    <li>Maintenance of the Avni application on the PlayStore</li>
+                    <li>Priority support on end-user applications</li>
+                    <li>Pre-installed community version of Metabase or Superset (choose one)</li>
+                </ol>
+                <p className="is-size-6 has-text-weight-semibold">Pricing Tiers</p>
+                <ol>
+                    <li>Up to 20 users: ₹7,000 per month (paid upfront annually)</li>
+                    <li>Up to 100 users: ₹10,000 per month (paid upfront annually)</li>
+                    <li>More than 100 users: Custom quote</li>
+                </ol>
+                <br/><br/>
             </div>
             <br/>
             <br/>
             <br/>
         </Layout>
-    )
+    );
 }
